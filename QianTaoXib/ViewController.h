0af4a64fe18a16fae229799f4bc07e61f11afb29
“郭辉” <guoh@lecuntao.com>
2017-03-02 10:44:33 +0800

    Initial Commit

---
+++ QianTaoXib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QianTaoXib
+//
+//  Created by guohui on 2017/3/2.
+//  Copyright © 2017年 guohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QianTaoXib/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QianTaoXib
+//
+//  Created by guohui on 2017/3/2.
+//  Copyright © 2017年 guohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
